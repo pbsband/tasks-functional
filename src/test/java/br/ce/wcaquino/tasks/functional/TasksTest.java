61329913533bd8006595289e83379708294bc175
unknown <pbsband@yahoo.com.br>
2020-06-08 18:59:33 -0300

    comentários das alterações

--- src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -17,11 +17,7 @@ public class TasksTest {
 	public WebDriver acessarAplicacao() throws MalformedURLException {
 //		WebDriver driver = new ChromeDriver();
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
-<<<<<<< HEAD
 		WebDriver driver = new RemoteWebDriver(new URL("http://10.30.1.4:4444/wd/hub"), cap);
-=======
-		WebDriver driver = new RemoteWebDriver(new URL("http://10.30.1.24:4444/wd/hub"), cap);
->>>>>>> b8f5cf33fdd94a6def422fc3465d93d39839c6c0
 		driver.navigate().to("http://10.30.1.19:8080/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
